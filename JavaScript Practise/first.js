a1c5e30500d0f02f3995056f79540b57c8bc0838
pallavi-kapila <pallavikapila01@gmail.com>
2025-12-02 22:30:44 +0530

    Refactor variable declarations , datatypes and add console logs for better clarity

--- JavaScript Practise/first.js
+++ JavaScript Practise/first.js
@@ -1,12 +1,31 @@
-fullName = "Tony Stark"
-$age = 45;
-var price = 99.99;
+let fullName = "Tony Stark"
+const $age = 45; 
+price = 99.99;
 let x = 10;
-const y = 20;
+let y = 20;
 var isFollow = true;
+const PI = 3.14;
 console.log(fullName);
 console.log($age);
 console.log(price);
 console.log(x);
 console.log(y);
 console.log(isFollow);
+console.log(PI);
+typeof fullName;
+typeof $age;
+typeof price;
+typeof x;
+typeof y;
+typeof isFollow;
+typeof PI;
+{
+    let city = "New York";
+    console.log(city);
+}
+{
+    let city = "Los Angeles";
+    console.log(city);
+    console.log(isFollow);
+    typeof city;
+}
