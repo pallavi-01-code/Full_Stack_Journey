55acebf3f53f82fe91af8f4ad10722b2c3289b45
pallavi-01-code <pallavikapila01@gmail.com>
2026-03-18 15:35:32 +0530

    Add additional function examples and comments in second.js

--- JavaScript Practise/second.js
+++ JavaScript Practise/second.js
@@ -2,7 +2,17 @@ var x1 = 7;
 
 function getName(){
     console.log("Hello JavaScript");
+    //it will copy the whole code in backend  
 }
 
 getName();
 console.log(x1);
+//console.log(b);
+
+var getName2 = function () {
+    //another way to write a function but it will act as a variable
+}
+
+var getName3 = () => {
+    //arrow function and here function will also act as a variable
+}
