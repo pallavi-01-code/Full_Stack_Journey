e145bc23cda2b4510dd48d95e93590838aa66e33
pallavi-01-code <pallavikapila01@gmail.com>
2026-03-30 23:19:29 +0530

    Add sixth.js and update index.html to reference it

---
+++ JavaScript Practise/sixth.js
@@ -0,0 +1,51 @@
+// var str = "Hello";
+// var str = "Yolo"
+
+// console.log(str);
+
+
+// let string = "How are you";
+
+// {
+// let string = "Thank You"
+// console.log(string);
+// }
+// console.log(string);
+
+// const strg = "Welcome";
+
+// console.log(strg)
+
+// {
+//     const strg = "I am good"
+//     console.log(strg);
+// }
+
+// const product = {
+//     name : "Parker Jotter Standard CT Ball Pen(Black)",
+//     rating: 4,
+//     dealOfTheDay: true,
+//     priceafterDis: 270,
+//     MRP: 285,
+//     totalDiscount : "5%"
+// }
+// console.log(product);
+
+
+let grades = prompt("Enter number out of 100: ")
+if(grades<=100 && grades >= 80){
+    console.log("A");
+}
+else if(grades<=89 && grades >= 70){
+    console.log("B");
+}
+else if(grades<=69 && grades >= 60){
+    console.log("C");
+}
+else if(grades<=59 && grades >= 50){
+    console.log("D");
+}
+else {
+    console.log("F")
+}
+console.log(grades);
