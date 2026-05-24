559fad9dc1567cdc3da5ec7d07ff949a77046ba7
pallavi-01-code <pallavikapila01@gmail.com>
2026-05-24 23:50:04 +0530

    Add JavaScript file for DOM manipulation examples and heading updates

---
+++ JavaScript Practise/33rd.js
@@ -0,0 +1,14 @@
+let div = document.querySelector("div");
+
+console.dir(div);
+console.dir(div.innerText)
+console.dir(div.innerHTML)
+console.dir(div.textContent)
+
+let heading = document.querySelector("h1");
+
+console.dir(heading);
+console.dir(heading.innerText = "New Heading");
+console.dir(heading.innerHTML = "<i>New Heading</i>");
+console.dir(heading.innerText);
+console.dir(heading.textContent);
