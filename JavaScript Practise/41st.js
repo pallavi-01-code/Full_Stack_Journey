ddd5614e79a6fabdbe3488dda9c79599e8ffd825
pallavi-01-code <pallavikapila01@gmail.com>
2026-06-04 23:53:26 +0530

    Add Circle class with area calculation and painting functionality

---
+++ JavaScript Practise/41st.js
@@ -0,0 +1,20 @@
+class Circle {
+   constructor(radius, color) {
+       this.radius = radius;  
+       this.color = color;
+   }
+
+   area() {
+      const area = this.radius * this.radius * Math.PI;
+       return area;
+   }
+   
+   paint() {
+			console.log(`Painting with color ${this.color}`);
+   }
+
+}
+
+const circle = new Circle(2, "red")
+const area = circle.area();
+console.log(area);
