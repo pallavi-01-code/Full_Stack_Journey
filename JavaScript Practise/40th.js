826889609546632f101493d09eb5536849dc3898
pallavi-01-code <pallavikapila01@gmail.com>
2026-06-04 23:52:23 +0530

    Refactor 40th.js to implement Rectangle class and calculate area

--- JavaScript Practise/40th.js
+++ JavaScript Practise/40th.js
@@ -1,7 +1,21 @@
-console.log(y);
-let a = 1900;
+class Rectangle {
+   constructor(width, height, color) {
+	    this.width = width;
+	    this.height = height;
+	    this.color = color; 
+   }
+   
+   area() {
+	   const area = this.width * this.height;
+		 return area;
+   }
+   
+   paint() {
+			console.log(`Painting with color ${this.color}`);
+   }
+   
+}
 
-const b =100;
-
-a =10;
-console.log(a);
+const rect = new Rectangle(2, 4)
+const area = rect.area();
+console.log(area)
